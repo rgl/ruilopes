20135222990ad91b49134dfa606f36d894ca208b
Rui Lopes <rgl@ruilopes.com>
2019-05-26 09:18:59 +0100

    init

---
+++ home.js
@@ -0,0 +1,16 @@
+function m(n, d) {
+	var c = ":l@m";
+	window.location.assign(c.charAt(3)+"a"+"i"+c.charAt(1)+"t"+"o"+c.charAt(0)+n+c.charAt(2)+d);
+}
+function script(url) {
+	var s = document.createElement('script');
+	s.async = true;
+	s.src = url;
+	document.documentElement.firstChild.appendChild(s);
+}
+var _gaq = [
+	['_setAccount', 'UA-11886181-1'],
+	['_setDomainName', '.ruilopes.com'],
+	['_trackPageview']
+];
+script(('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js');
